5e750d6573ba13825165580b1724010a956dcb5f
Maryam Mirafzal/ <maryammirafzal9@gmail.com>
2024-11-16 17:32:38 +0330

    [ADD] Create and Connecting DB
    [+] Add Layout
    [+] Creating a DB and Connecting the Program to it
    [+] Create Customer Model
    [+] Add API with Post Method to Get Data

---
+++ components/layout/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import styles from "../../styles/gl"
+
+function Layout({children}) {
+  return (
+    <>
+       <header className={styles.header}>
+        <h2>Botostart CRM</h2>
+       </header>
+       <div className={styles.main}>
+         {children}
+       </div>
+       <footer className={styles.footer}>
+         <p>Next.js Cours | CRM Project &copy;</p>
+       </footer>
+    </>
+  )
+}
+
+export default Layout
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import Layout from '../components/layout/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <Layout>
+    1 <Component {...pageProps} />
+  </Layout>
 }
 
 export default MyApp
